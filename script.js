7bfb352def11ae2e0cf3cf1e57e5fd5a116f8f8f
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-08-22 20:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
+	// Get all <strong> elements in the document
+    const strongElements = document.querySelectorAll('strong');
 
+    // Loop through each <strong> element and change its color to green
+    strongElements.forEach(element => {
+        element.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
+	// Get all <strong> elements in the document
+    const strongElements = document.querySelectorAll('strong');
 
+    // Loop through each <strong> element and change its color back to black
+    strongElements.forEach(element => {
+        element.style.color = 'black';
+    });
     
 }
